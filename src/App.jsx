d0030de3426927b66f8c27cec1b5602ab3304797
Sophia9156 <suhyun9156@gmail.com>
2022-02-14 21:03:49 +0900

    clone-youtube 완성

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import VideoList from './components/video_list/Video_list';
 import SearchHeader from './components/search_header/Search_header';
+import VideoDetail from './components/video_detail/Video_detail';
 import styles from './app.module.css';
 
 function App({youtube}) {
   const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
+
+  const selectVideo = (video) => {
+    setSelectedVideo(video);
+  }
 
   const search = query => {
     youtube
     .search(query)
-    .then(videos => setVideos(videos))
+    .then(videos => {
+      setVideos(videos);
+      setSelectedVideo(null);
+    });
   }
 
   useEffect(() => {
@@ -21,7 +30,20 @@ function App({youtube}) {
   return (
     <div className={styles.app}>
       <SearchHeader onSearch={search} />
-      <VideoList videos={videos}/>
+      <section className={styles.content}>
+        {selectedVideo && (
+        <div className={styles.detail}>
+          <VideoDetail video={selectedVideo} />
+        </div>
+        )}
+        <div className={styles.list}>
+          <VideoList 
+          videos={videos} 
+          onVideoClick={selectVideo} 
+          display={selectedVideo ? 'list' : 'grid'} 
+          />
+        </div>
+      </section>
     </div>
   );
 }
--- src/components/video_item/Video_item.jsx
+++ src/components/video_item/Video_item.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import styles from './video_item.module.css';
 
-const VideoItem = (props) => {
-  const video = props.video.snippet
+const VideoItem = ({video, video: {snippet}, onVideoClick, display}) => {
+  const displayType = display === 'list' ? styles.list : styles.grid;
   return(
-    <li className={styles.container}>
+    <li className={`${styles.container} ${displayType}`} onClick={() => onVideoClick(video)}>
       <div className={styles.video}>
-        <img className={styles.thumbnail} src={video.thumbnails.medium.url} alt="video thumbnail" />
+        <img className={styles.thumbnail} src={snippet.thumbnails.medium.url} alt="video thumbnail" />
         <div className={styles.metadata}>
-          <p className={styles.title}>{video.title}</p>
-          <p className={styles.channel}>{video.channelTitle}</p>
+          <p className={styles.title}>{snippet.title}</p>
+          <p className={styles.channel}>{snippet.channelTitle}</p>
         </div>
       </div>
     </li>
